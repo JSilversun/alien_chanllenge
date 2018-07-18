56f66a2682a6f95dcaf0354e31703bba2f067010
Julia Suarez <ju.threw@gmail.com>
2018-07-18 10:50:58 -0400

    Se arreglo problemas con los input

--- alien.py
+++ alien.py
@@ -47,18 +47,21 @@ def main():
     fname="input.txt"
     lines=[line.rstrip('\r\n') for line in open(fname)]
     n=int(lines[0])
+    i=1
+    nships=0
     # Reading file
-    for i in range(1,n+1):
+    while nships<n:
         ship_dimensions=[float(x) for x in lines[i].split()]
         ship_width=int(ship_dimensions[1])
         ship_height=int(ship_dimensions[0])
         scale=ship_dimensions[2]
         command_centers=[]
         m = [[0 for x in range(ship_width)] for y in range(ship_height)] 
-        for row in range(0,ship_width):
-            line=re.sub('[^a-zA-Z]+', ' ', lines[row+i+1])
-            line=line.split()
-            for col in range(0,len(line)):
+        i+=1
+        for row in range(0,ship_height):
+            line=lines[i].split()
+            i+=1
+            for col in range(0,ship_width):
                 m[row][col]=line[col]
                 current_center=m[row][col]
                 command_center=next((_ for _ in command_centers if _.name==current_center),None)
@@ -67,32 +70,33 @@ def main():
                     command_centers.append(CommandCenter(current_center, row, col,scale))
                 else:
                     command_center.check_bounds(current_center, row, col)
-
-    # Algorithm      
-    result=""
-    command_centers.sort(key=lambda x: x.missing_cells)
-    i=0
-    limit=100
-    while command_centers>0 and i<limit:
-        i+=1
-        result+=destroy_layer(m,command_centers)
-        command_centers=[_ for _ in command_centers if _.missing_cells!=0]  
-        for command_center in command_centers:
-            stop=False
-            for row in range(command_center.last_y,command_center.max_y+1):
-                for col in range(command_center.last_x,command_center.max_x+1):
-                    if m[row][col]==command_center.name or m[row][col]=='-':
-                        command_center.check_cell(m[row][col],row,col)
-                    else:
-                        command_center.last_x=col
-                        command_center.last_y=row
-                        stop=True
+        
+        # Algorithm      
+        result=""
+        command_centers.sort(key=lambda x: x.missing_cells)
+        it=0
+        limit=100
+        while command_centers>0 and it<limit:
+            it+=1
+            result+=destroy_layer(m,command_centers)
+            command_centers=[_ for _ in command_centers if _.missing_cells!=0]  
+            for command_center in command_centers:
+                stop=False
+                for row in range(command_center.last_y,command_center.max_y+1):
+                    for col in range(command_center.last_x,command_center.max_x+1):
+                        if m[row][col]==command_center.name or m[row][col]=='-':
+                            command_center.check_cell(m[row][col],row,col)
+                        else:
+                            command_center.last_x=col
+                            command_center.last_y=row
+                            stop=True
+                            break
+                        if command_center.max_x==command_center.max_x:
+                            command_center.last_x=command_center.min_x
+                    if stop:
                         break
-                    if command_center.max_x==command_center.max_x:
-                        command_center.last_x=command_center.min_x
-                if stop:
-                    break
-    print(result)
+        print(result)
+        nships+=1
 
 main()
 
